0eb6ce14b06997798893dd5c0883d02f38de14a3
YK <774064425@qq.com>
2018-04-15 21:23:52 +0800

    提交taxi作业

--- main/main.js
+++ main/main.js
@@ -1,4 +1,26 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(waitMinites,kilometers) {
+    let waitFees = wait(waitMinites);
+    let runFees = runFee(kilometers);
+    let fee = Math.round(waitFees + runFees);
+    let feeCheck = 
+    `账单明细：
+    等待时间：${waitMinites}（分钟），收费：${waitFees.toFixed(2)}（元）；
+    乘坐距离：${kilometers}（公里），收费：${runFees.toFixed(2)}（元）；
+    总计收费：${fee.toFixed(2)}（元）。`
+    console.log(feeCheck);
 };
+
+let wait = (waitMinites) => {
+    return waitFee = Math.ceil(waitMinites) * 0.25;
+}
+
+let runFee = (kilometers) => {
+    if (kilometers <= 2) {
+        return runFee = 6.00;
+    } else if (kilometers > 2 && kilometers <= 8) {
+        return runFee = 6.00 + 0.80 * (Math.ceil(kilometers - 2));
+    } else {
+        return runFee = 6 + 0.80 * 6 + 1.20 * (Math.ceil(kilometers - 8))
+    }
+}
+
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,4 +2,61 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
+    var waitMinites;
+    var kilometers;
+
+    it('should print correct feeCheck1', function () {
+
+        // waitMinites = 2;
+        // kilometers = 2;
+
+        spyOn(console, 'log');
+
+        main(2,2);
+
+        var expectText1 =
+            `账单明细：
+    等待时间：2（分钟），收费：0.50（元）；
+    乘坐距离：2（公里），收费：6.00（元）；
+    总计收费：7.00（元）。`;
+
+        expect(console.log).toHaveBeenCalledWith(expectText1);
+    });
+
+    // it('should print correct feeCheck2', function () {
+
+    //     waitMinites = 0;
+    //     kilometers = 5.6;
+
+    //     spyOn(console, 'log');
+
+    //     main(waitMinites, kilometers);
+
+    //     var expectText2 =
+    //         `账单明细：
+    // 等待时间：0（分钟），收费：0.00（元）；
+    // 乘坐距离：5.6（公里），收费：9.20（元）；
+    // 总计收费：9.00（元）。`;
+
+    //     expect(console.log).toHaveBeenCalledWith(expectText2);
+    // });
+
+
+    // it('should print correct feeCheck3', function () {
+
+    //     waitMinites = 10;
+    //     kilometers = 10;
+
+    //     spyOn(console, 'log');
+
+    //     main(waitMinites, kilometers);
+
+    //     var expectText3 =
+    //         `账单明细：
+    // 等待时间：10（分钟），收费：2.50（元）；
+    // 乘坐距离：10（公里），收费：13.20（元）；
+    // 总计收费：16.00（元）。`;
+
+    //     expect(console.log).toHaveBeenCalledWith(expectText3);
+    // });
 });
